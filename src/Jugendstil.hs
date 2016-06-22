33edb3a7f0237c4e5974ae8f11382b5031f50d9e
Fumiaki Kinoshita <fumiexcel@gmail.com>
2016-06-22 17:57:33 +0900

    Remove Jugendstil.Widget

--- src/Jugendstil.hs
+++ src/Jugendstil.hs
@@ -1,11 +1,9 @@
 module Jugendstil
-  ( module Jugendstil.Widget
-  , module Jugendstil.Doc
+  ( module Jugendstil.Doc
   , module Jugendstil.Color
   , Box(..)
   ) where
 
-import Jugendstil.Widget
 import Jugendstil.Doc
 import Jugendstil.Color
 import Data.BoundingBox
